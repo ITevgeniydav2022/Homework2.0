8465f11ee0c8adbacd56cce5558c744311ea044f
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-10 15:48:05 +0300

    Сделано ДЗ Переменные ч.2 задание 2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,26 @@ public class Main {
         System.out.println(" Значение пременной boxerWeight с типом float = " + boxerWeight);
         double landingPoint = 23.5555555555;
         System.out.println(" Значение пременной landingPoint с типом double = " + landingPoint);
+
+        // Задание 2
+        System.out.println("Задание 2");
+        float a = 27.12f;
+        System.out.println(" Значение пременной A с типом float = " + a);
+        byte b = 2;
+        System.out.println(" Значение пременной B с типом byte = " + b);
+        short c = 786;
+        System.out.println(" Значение пременной C с типом short = " + c);
+        boolean d = false;
+        System.out.println(" Значение пременной D с типом boolean = " + d);
+        short e = 569;
+        System.out.println(" Значение пременной E с типом short = " + e);
+        short f = - 159;
+        System.out.println(" Значение пременной F с типом short = " + f);
+        short g = 27897;
+        System.out.println(" Значение пременной G с типом short = " + g);
+        double i = 67.;
+        System.out.println(" Значение пременной I с типом double = " + i);
+        long h = 987678965549L;
+        System.out.println(" Значение пременной H с типом long = " + h);
     }
 }
